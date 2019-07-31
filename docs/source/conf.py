040465980586b5ed7007c33274b67aeeb8afac11
Fridolin Pokorny <fridolin@redhat.com>
2019-07-31 10:25:14 +0200

    Substitute copy-paste adviser leftovers in docs configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -107,7 +107,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'ThothAdviserdoc'
+htmlhelp_basename = 'ThothPackageAnalyzerdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -134,7 +134,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'ThothAdviser.tex', 'Thoth Adviser Documentation',
+    (master_doc, 'ThothPackageAnalyzer.tex', 'Thoth Package Analyzer Documentation',
      'Thoth team', 'manual'),
 ]
 
@@ -144,7 +144,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'thothadviser', 'Thoth Adviser Documentation',
+    (master_doc, 'thothpackageanalyzer', 'Thoth Package Analyzer Documentation',
      [author], 1)
 ]
 
@@ -155,8 +155,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'ThothAdviser', 'Thoth Adviser Documentation',
-     author, 'ThothAdviser', 'One line description of project.',
+    (master_doc, 'ThothPackageAnalyzer', 'Thoth Package Analyzer Documentation',
+     author, 'ThothPackageAnalyzer', 'One line description of project.',
      'Miscellaneous'),
 ]
 
